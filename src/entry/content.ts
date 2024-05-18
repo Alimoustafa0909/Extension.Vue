31f6c5f0a41ddb0b9a3e75aeb6528ad6226baaba
Alimoustafa0909 <122943649+Alimoustafa0909@users.noreply.github.com>
2024-05-19 00:34:27 +0300

    FormFinder

--- src/entry/content.ts
+++ src/entry/content.ts
@@ -3,6 +3,7 @@ import ContentScript from '../view/ContentScript.vue';
 
 // Create a new div element to mount our Vue component
 const mountElement = document.createElement('div');
+mountElement.id ="FormFinder"
 document.body.appendChild(mountElement);
 
 // Create and mount the Vue app
